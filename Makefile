9f2f2945f40d798c35dbd5597591d1a15bbce127
Dmitry Ponyatov <dponyatov@gmail.com>
2024-11-13 16:53:01 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- Makefile
+++ Makefile
@@ -4,4 +4,12 @@ include mk/cross.mk
 include mk/dirs.mk
 include mk/tool.mk
 include mk/package.mk
+include mk/src.mk
+include mk/cfg.mk
+include mk/all.mk
+include mk/format.mk
+include mk/rule.mk
+include mk/doc.mk
+include mk/install.mk
 include mk/ref.mk
+include mk/merge.mk
--- mk/ref.mk
+++ mk/ref.mk
@@ -6,5 +6,11 @@ REF += $(GZ)/$(CUBEMX_GZ)
 $(GZ)/$(CUBEMX_GZ):
 	$(CURL) $@ $(CUBEMX_URL)/$(CUBEMX_GZ)
 
+https://github.com/STMicroelectronics/STM32CubeF0/archive/refs/tags/v$(CUBEF0_VER).zip
+https://github.com/STMicroelectronics/STM32CubeF1/archive/refs/tags/v$(CUBEF1_VER).zip
+https://github.com/STMicroelectronics/STM32CubeF3/archive/refs/tags/v$(CUBEF3_VER).zip
+https://github.com/STMicroelectronics/STM32CubeF4/archive/refs/tags/v$(CUBEF4_VER).zip
+https://github.com/STMicroelectronics/STM32CubeH7/archive/refs/tags/v$(CUBEH7_VER).zip
+
 .PHONY: ref
 ref: $(REF)
