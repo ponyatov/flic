d387cee4a5e5cba8be5258ef2200f8199f62b661
Dmitry Ponyatov <dponyatov@gmail.com>
2024-11-13 21:27:20 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- arch/CortexM.mk
+++ arch/CortexM.mk
@@ -1,2 +1,46 @@
 TARGET = arm-none-eabi
 APT   += gcc-$(TARGET) gdb-multiarch stlink-tools
+
+# tool: CubeMX binary path
+CUBEMX ?= $(HOME)/STM32/CubeMX/STM32CubeMX
+GZ     += $(CUBEMX)
+
+# run make cubemx
+cubemx: $(CUBEMX)
+	$<
+
+# CubeMX automated install
+CUBE_SDIR  = tmp/cubemx
+CUBE_SETUP = SetupSTM32CubeMX-$(subst -,.,$(CUBEMX_VER))
+
+$(CUBEMX): $(CUBE_SDIR)/$(CUBE_SETUP)
+	echo $@ $<
+
+$(CUBE_SDIR)/$(CUBE_SETUP): $(CROSS)/$(CUBEMX_GZ)
+	unzip -d $(CUBE_SDIR) $< && touch $@
+
+GZ += $(CROSS)/$(CUBEMX_GZ)
+$(CROSS)/$(CUBEMX_GZ):
+	$(CURL) $@ $(CUBEMX_URL)/$(CUBEMX_GZ)
+
+# REF += ref/STM32CubeF0/README.md
+# ref/STM32CubeF0/README.md: $(CROSS)/$(CUBEF0_GZ)
+# GZ += $(CROSS)/$(CUBEF0_GZ)
+# $(CROSS)/$(CUBEF0_GZ):
+# 	$(CURL) $@ $(CUBEF0_URL)/v$(CUBEF0_VER).zip
+
+# # ref/STM32CubeF1/README.md: $(CROSS)/$(CUBEF1_GZ)
+# # $(CROSS)/$(CUBEF1_GZ):
+# # 	$(CURL) $@ https://github.com/STMicroelectronics/STM32CubeF1/archive/refs/tags/v$(CUBEF1_VER).zip
+
+# # ref/STM32CubeF3/README.md: $(CROSS)/$(CUBEF0_GZ)
+# # $(CROSS)/$(CUBEF0_GZ):
+# # 	$(CURL) $@ https://github.com/STMicroelectronics/STM32CubeF3/archive/refs/tags/v$(CUBEF3_VER).zip
+
+# # ref/STM32CubeF0/README.md: $(CROSS)/$(CUBEF0_GZ)
+# # $(CROSS)/$(CUBEF0_GZ):
+# # 	$(CURL) $@ https://github.com/STMicroelectronics/STM32CubeF4/archive/refs/tags/v$(CUBEF4_VER).zip
+
+# # ref/STM32CubeF0/README.md: $(CROSS)/$(CUBEF0_GZ)
+# # $(CROSS)/$(CUBEF0_GZ):
+# # 	$(CURL) $@ https://github.com/STMicroelectronics/STM32CubeH7/archive/refs/tags/v$(CUBEH7_VER).zip
--- mk/ref.mk
+++ mk/ref.mk
@@ -1,29 +1,3 @@
 REF += ref/microrl/README
 ref/microrl/README:
 	$(GITREF) -b master https://github.com/Helius/microrl.git $(dir $@)
-
-GZ += $(CROSS)/$(CUBEMX_GZ)
-$(CROSS)/$(CUBEMX_GZ):
-	$(CURL) $@ $(CUBEMX_URL)/$(CUBEMX_GZ)
-
-REF += ref/STM32CubeF0/README.md
-ref/STM32CubeF0/README.md: $(CROSS)/$(CUBEF0_GZ)
-GZ += $(CROSS)/$(CUBEF0_GZ)
-$(CROSS)/$(CUBEF0_GZ):
-	$(CURL) $@ $(CUBEF0_URL)/v$(CUBEF0_VER).zip
-
-# ref/STM32CubeF1/README.md: $(CROSS)/$(CUBEF1_GZ)
-# $(CROSS)/$(CUBEF1_GZ):
-# 	$(CURL) $@ https://github.com/STMicroelectronics/STM32CubeF1/archive/refs/tags/v$(CUBEF1_VER).zip
-
-# ref/STM32CubeF3/README.md: $(CROSS)/$(CUBEF0_GZ)
-# $(CROSS)/$(CUBEF0_GZ):
-# 	$(CURL) $@ https://github.com/STMicroelectronics/STM32CubeF3/archive/refs/tags/v$(CUBEF3_VER).zip
-
-# ref/STM32CubeF0/README.md: $(CROSS)/$(CUBEF0_GZ)
-# $(CROSS)/$(CUBEF0_GZ):
-# 	$(CURL) $@ https://github.com/STMicroelectronics/STM32CubeF4/archive/refs/tags/v$(CUBEF4_VER).zip
-
-# ref/STM32CubeF0/README.md: $(CROSS)/$(CUBEF0_GZ)
-# $(CROSS)/$(CUBEF0_GZ):
-# 	$(CURL) $@ https://github.com/STMicroelectronics/STM32CubeH7/archive/refs/tags/v$(CUBEH7_VER).zip
