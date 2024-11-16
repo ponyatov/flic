360b5571517d02f3a9a0c5b1474ab3c72c9a9312
Dmitry Ponyatov <dponyatov@gmail.com>
2024-11-16 09:57:55 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ cmake/i686-w64-mingw32-win32.cmake
@@ -0,0 +1,23 @@
+# https://github.com/jopohl/sdrbuild/blob/master/mingw-w64-i686.cmake
+
+# toolchain file for cross-compiling for retro Windows x32 (XP, 7)
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_BUILD_TYPE  Debug)
+set(TOOLCHAIN_PREFIX  i686-w64-mingw32)
+
+# cross compilers to use for C and C++
+set(CMAKE_C_COMPILER   ${TOOLCHAIN_PREFIX}-gcc-win32)
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++-win32)
+set(CMAKE_RC_COMPILER  ${TOOLCHAIN_PREFIX}-windres)
+# set(CMAKE_MC_COMPILER  ${TOOLCHAIN_PREFIX}-windmc)
+
+# target environment on the build host system
+#   set 1st to dir with the cross compiler's C/C++ headers/libs
+set(CMAKE_FIND_ROOT_PATH /usr/${TOOLCHAIN_PREFIX})
+
+# modify default behavior of FIND_XXX() commands to
+# search for headers/libs in the target environment and
+# search for programs in the build host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
