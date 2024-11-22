2ca60cc93aaa50caa1f53e362dcdf164065270c6
Dmitry Ponyatov <dponyatov@gmail.com>
2024-11-23 02:54:32 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/rust.mk
+++ mk/rust.mk
@@ -2,10 +2,10 @@ R += $(wildcard src/*.rs)
 B  = $(patsubst src/%.rs,bin/%,$(R))
 
 .PHONY: rust
-rust: bin/$(MODULE)_rs
+rust: bin/FORTH
 	$^
 
-bin/$(MODULE)_rs: $(R)
+bin/FORTH: $(R) lib/FORTH.ini
 	rustc -o $@ $<
 
 tmp/format_rs: $(R)
--- src/FORTH.rs
+++ src/FORTH.rs
@@ -1,6 +1,9 @@
 const ABOUT: &str = "FORTH.rs: tiny pure REPL/CLI system in Rust (embedded)";
 const AUTHOR: &str = "(c) Dmitry Ponyatov <dponyatov@gmail.com> 2024 MIT";
 
+const INI: &'static str = include_str!("../lib/FORTH.ini");
+
 fn main() {
     println!("\n{}\n{}\n", ABOUT, AUTHOR);
+    println!("{}", INI);
 }
