cd63f03348fa63be3c9475a2bcf6216a6fec5494
Dmitry Ponyatov <dponyatov@gmail.com>
2024-11-23 02:46:31 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/FORTH.rs
+++ src/FORTH.rs
@@ -1,3 +1,6 @@
-// tiny pure FORTH system in Rust (embedded)
+const ABOUT: &str = "FORTH.rs: tiny pure REPL/CLI system in Rust (embedded)";
+const AUTHOR: &str = "(c) Dmitry Ponyatov <dponyatov@gmail.com> 2024 MIT";
 
-fn main() {}
+fn main() {
+    println!("\n{}\n{}\n", ABOUT, AUTHOR);
+}
