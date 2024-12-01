0a94663ab24a219d664d5f71b387e4d7d8b57c76
Dmitry Ponyatov <dponyatov@gmail.com>
2024-12-02 00:18:52 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/ref.mk
+++ mk/ref.mk
@@ -2,6 +2,10 @@ REF += ref/Espruino/README.md
 ref/Espruino/README.md:
 	$(GITREF) https://github.com/espruino/Espruino.git $(dir $@)
 
+REF += ref/iskrajs/README.md
+ref/iskrajs/README.md:
+	$(GITREF) -b iskrajs https://github.com/amperka/Espruino.git $(dir $@)
+
 REF += ref/stm32f4discovery-bsp/README.md
 ref/stm32f4discovery-bsp/README.md:
 	$(GITREF) https://github.com/STMicroelectronics/stm32f4discovery-bsp.git $(dir $@)
