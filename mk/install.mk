1feaa5c9b47d8231587d61041254e90593f304af
Dmitry Ponyatov <dponyatov@gmail.com>
2024-12-12 01:52:27 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/install.mk
+++ mk/install.mk
@@ -3,6 +3,7 @@
 install: doc ref gz
 	$(MAKE) update
 	rustup target add thumbv7em-none-eabihf
+	rustup component add rustfmt
 update:
 	sudo apt update
 	sudo apt install -uy `cat apt.$(shell lsb_release -si)` $(APT)
