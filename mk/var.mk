507aa3f52a5af51a4f26d4ef58c2a2d731cb55e3
Dmitry Ponyatov <dponyatov@gmail.com>
2024-12-14 13:54:11 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/var.mk
+++ mk/var.mk
@@ -1,5 +1,5 @@
 # var
 MODULE = $(notdir $(CURDIR))
-REL    = $(shell git rev-parse --short=4    HEAD)
+REL    = $(shell git rev-parse --short=5    HEAD)
 BRANCH = $(shell git rev-parse --abbrev-ref HEAD)
-NOW    = $(shell date +%d%m%y)
+NOW    = $(shell date +%y%m%d)
